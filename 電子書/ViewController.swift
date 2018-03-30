a42d593575848ee21d30c9b59c0c2d6d69c2fcdf
Robson <robson750125@icloud.com>
2018-03-30 14:59:28 +0800

    Initial Commit

---
+++ 電子書/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  電子書
+//
+//  Created by Robson Hsieh on 2018/3/30.
+//  Copyright © 2018年 Robson Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
